04923a01260d10176f432100026d347826048c86
KarenY <karenyupro1@gmail.com>
2020-12-28 18:34:59 -0500

    updated incident.py

--- src/models/incident.py
+++ src/models/incident.py
@@ -9,4 +9,32 @@ class Incident():
         for k, v in kwargs.items():
             setattr(self, k, v)
 
+ @classmethod
+    def find_by_incident_num(self, incident_num, cursor):
+        incident_query = """SELECT * FROM incidents WHERE incident_num = %s"""
+        cursor.execute(incident_query, (incident_num,))
+        record =  cursor.fetchone()
+        return db.build_from_record(models.Incident, record)
+
+    def complaints(self,  cursor):
+        offense_query = """SELECT complaints.* FROM complaints WHERE incidents.id = %s"""
+        cursor.execute(offense_query, (self.id,))
+        record = cursor.fetchone()
+        return db.build_from_record(models.Complaint, record)
+
+    def find_by_borough(self, cursor):
+        borough_query = """SELECT locations.* FROM locations WHERE locations.id 
+         = %s"""
+        cursor.execute(categories_query, (self.id,))
+        venue_records = cursor.fetchall()
+        return db.build_from_records(models.Category, venue_records
+
+    
+    def to_json(self, cursor):
+        venue_json = self.__dict__
+        location = self.location(cursor)
+        if location:
+            location_dict = {'lon': location.longitude, 'lat': location.latitude, 'address': location.address}
+            venue_json['location'] = location_dict
+        return venue_json
 
